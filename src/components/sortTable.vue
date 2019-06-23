3ba072fe73ca01e2a9afdfc3eca721750ad04680
qiyue <292019984@qq.com>
2019-06-24 01:35:39 +0800

    222

--- src/components/sortTable.vue
+++ src/components/sortTable.vue
@@ -22,15 +22,15 @@
     </el-table>
 
     <button
-      style="width:500px;height:500px;border:0px solid blue;position:relative;display:block;background:orange;outline: none;"
+      style="width:500px;height:500px;border:1px solid blue;position:relative;display:block;background:orange;outline: none;"
       @click.stop="test()"
       @keyup.ctrl.86="ctrlv"
     >
       <!-- <vue-draggable-resizable :grid="[20,20]" :parent="false">
         <div style="width:100%;height:100%;background:red" @contextmenu.prevent="contextmenu">右击</div>
-      </vue-draggable-resizable> -->
+      </vue-draggable-resizable>-->
 
-      <vue-draggable-resizable :parent="false" :grid="[20,20]">
+      <vue-draggable-resizable :grid="[20,20]" :parent="false">
         <div
           @click.stop="kk()"
           @dblclick="dblclick()"
@@ -43,7 +43,7 @@
             @keyup.ctrl.67="ctrlc"
             style="width:100%;height:100%;display:block;background:white;border:0px solid blue;"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" style="width:100%;height:100%;">
+            <!-- <svg xmlns="http://www.w3.org/2000/svg" version="1.1" style="width:100%;height:100%;">
               <rect
                 x="50"
                 y="20"
@@ -54,7 +54,18 @@
                 style="fill:blue;stroke:pink;stroke-width:5;fill-opacity:1;
           stroke-opacity:0.9"
               ></rect>
-            </svg>
+            </svg>-->
+            <div style="position:absolute;top:0px;left:0px;width:100%">
+              <quill-editor
+                v-model="content"
+                ref="myQuillEditor"
+                :options="editorOption"
+                @blur="onEditorBlur($event)"
+                @focus="onEditorFocus($event)"
+                @change="onEditorChange($event)"
+                v-if="quill_editor"
+              ></quill-editor>
+            </div>
           </button>
         </div>
       </vue-draggable-resizable>
@@ -67,7 +78,38 @@ export default {
   data () {
     return {
       color1: '',
+      content: '<p><span style="color: rgb(230, 0, 0);">ffdfd</span></p>',
+
+      editorOption: {
+        modules: {
+          toolbar: [
+            ["bold", "italic", "underline", "strike"],
+            // 加粗 斜体 下划线 删除线
+            ["code-block"],
+            // 引用  代码块
+            // [{ header: 1 }, { header: 2 }], // 1、2 级标题你怕吗
+            [{ list: "ordered" }, { list: "bullet" }],
+            // 有序、无序列表
+            // [{ script: "sub" }, { script: "super" }], // 上标/下标
+            // [{ indent: "-1" }, { indent: "+1" }], // 缩进
+            // [{'direction': 'rtl'}],                         // 文本方向
+            [{ size: ["small", false, "large", "huge"] }],
+            // 字体大小
+            // [{ header: [1, 2, 3, 4, 5, 6, false] }], // 标题
+            [{ color: [] }, { background: [] }],
+            // 字体颜色、字体背景颜色
+            // [{ font: [] }], // 字体种类
+            [{ align: [] }],
+            // 对齐方式
+            // ["clean"], // 清除文本格式
+            ["image"]
+            // 链接、图片、视频
+
+          ]
+        }
+      },
       shoucolor_picker: false,
+      quill_editor: false,
       tableData: [{
         hangdle: '22',
         date: '2016-05-02',
@@ -103,6 +145,18 @@ export default {
     ctrlv () {
       console.log('ctrlv')
     },
+    onEditorBlur () {
+      console.log('失去焦点事件')
+
+    },
+    onEditorFocus () {
+      console.log('获得焦点事件')
+    },
+    onEditorChange () {
+      console.log('内容改变事件')
+      console.log('content', this.content)
+    },
+
     formatter (row, column) {
       return row.address
     },
@@ -113,6 +167,7 @@ export default {
     contextmenu () {
       console.log('contextmenu')
       this.shoucolor_picker = true
+
     },
     test () {
       console.log('contextmenu')
@@ -126,6 +181,7 @@ export default {
     },
     dblclick () {
       console.log('dblclick')
+      this.quill_editor = true
     }
   }
 }
@@ -138,6 +194,13 @@ export default {
   position: relative;
 }
 .box {
-  transform: rotate(0deg);
+  transform: rotate3d(0, 0, 1, 0deg);
+  font-size: 30px;
 }
 </style>
+<style >
+.ql-container.ql-snow {
+  border: 0px solid #ccc;
+}
+</style>
+
--- src/main.js
+++ src/main.js
@@ -15,6 +15,11 @@ import VXETable from 'vxe-table'
 import 'vxe-table/lib/index.css'
 import 'vue-draggable-resizable/dist/VueDraggableResizable.css'
 import VueDraggableResizable from 'vue-draggable-resizable'
+import VueQuillEditor from 'vue-quill-editor'
+// require styles 引入样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 // import axios from 'axios'
 Vue.prototype.$http = axios
 Vue.prototype.VueDraggableResizable = VueDraggableResizable
@@ -26,6 +31,7 @@ Vue.config.productionTip = false
 Vue.component('vue-draggable-resizable', VueDraggableResizable)
 Vue.use(VXETable)
 Vue.use(Viewer)
+Vue.use(VueQuillEditor)
 // 这里是插件的默认设置
 Viewer.setDefaults({
   zIndexInline: 9999
