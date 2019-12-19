8d40986b25be28fbd2f9f1cd7ec783ab623f1a3a
qiyue <292019984@qq.com>
2019-12-19 23:42:57 +0800

    6666

--- src/components/graph.vue
+++ src/components/graph.vue
@@ -1,174 +1,51 @@
 <template>
-  <div class="page">
-    <el-button type="text" @click="seletetype('circle')">圆形FDF</el-button>
-    <el-button type="text" @click="seletetype('triangle')">箭头三角形</el-button>
-    <div style="display:flex">
-      <div
-        @mousedown="getMousePos('mousedown')"
-        @mousemove="getMousePos('mousemove')"
-        @mouseup="getMousePos('up')"
-        style="width:1000px;height:800px;background:red"
-      >
-        <div
-          class="content"
-          :style="{width:xx+'px',height:yy+'px',left:cx+'px',top:cy+'px',transform:'rotate('+rotate+'deg)'}"
-        >
-          <svg version="1.1" style="background:'';width:100%;height:100%;">
-            <circle
-              cx="50.5"
-              cy="50"
-              r="50"
-              stroke="black"
-              stroke-width="1"
-              fill="white"
-              v-if="ShapeType==='circle'"
-              :style="{transform:'scale('+xx/100+','+yy/100+')'}"
-            />
+  <div>
+    <el-upload
+      class="upload-demo"
+      ref="upload"
+      action="https://jsonplaceholder.typicode.com/posts/"
+      :on-preview="handlePreview"
+      :on-remove="handleRemove"
+      :file-list="fileList"
+      :on-change="change"
+      :auto-upload="false"
+    >
+      <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
 
-            <polygon
-              points="0,30 50,30 50,0 100,50 50,100 50,70 0,70 "
-              stroke="black"
-              stroke-width="0.5"
-              fill="white"
-              v-if="ShapeType==='triangle'"
-              :style="{transform:'scale('+xx/100+','+yy/100+')'}"
-            />
-          </svg>
-        </div>
-      </div>
-    </div>
+      <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+    </el-upload>
+    <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
   </div>
 </template>
 
-<script type="text/ecmascript-6">
+
+<script>
 export default {
   data () {
     return {
-
-      name: '',
-      password: '',
-      cx: 0,
-      cy: 0,
-      mousemovex: 0,
-      mousemovey: 0,
-      xx: 0,
-      yy: 0,
-      active: false,
-      ShapeType: '',
-      num: 0,
-      arrt: [],
-      scalex: 3,
-      scaley: 5,
-      rotate: 0
-
-    }
-  },
-  components: {
-
+      fileList: [{ name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100' }, { name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100' }]
+    };
   },
   methods: {
-    add (n) {
-      if (n === 1) {
-        const ll = {
-          id: this.num,
-          text: 'nihao',
-          type: 'u'
-        }
-        this.arrt.push(ll)
-        this.num++
-      } else if (n === 2) {
-        const ll = {
-          id: this.num,
-          text: 'ni',
-          type: 'o'
-        }
-        this.arrt.push(ll)
-        this.num++
-        console.log(this.arrt)
+    submitUpload () {
+      this.$refs.upload.submit();
+    },
+    handleRemove (file, fileList) {
+      // console.log(file, fileList);
+    },
+
+    change (file) {
+      console.log(file, 333333333);
+      if (file.name == "u=1172165602,1835764564&fm=26&gp=0.jpg") {
+        alert(file)
+      } else {
+        this.$refs.upload.submit();
+        
       }
     },
-    kk () {
-      console.log(22222)
-    },
-    seletetype (type) {
-      this.ShapeType = type
-      console.log('this.ShapeType', this.ShapeType)
-    },
-    getMousePos (type) {
-      if (this.ShapeType !== '') {
-        if (type === 'mousedown') {
-          const e = event || window.event
-          // const scrollX = document.documentElement.scrollLeft || document.body.scrollLeft
-          // const scrollY = document.documentElement.scrollTop || document.body.scrollTop
-          // const x = e.pageX || e.clientX + scrollX
-          // const y = e.pageY || e.clientY + scrollY
-          const x = e.offsetX
-          const y = e.offsetY
-          console.log(x)
-          console.log(y)
-          this.cx = x
-          this.cy = y
-          this.active = true
-        } else if (type === 'mousemove' && this.active === true) {
-          const e = event || window.event
-          // const scrollX = document.documentElement.scrollLeft || document.body.scrollLeft
-          // const scrollY = document.documentElement.scrollTop || document.body.scrollTop
-          // const x = e.pageX || e.clientX + scrollX
-          // const y = e.pageY || e.clientY + scrollY
-          const x = e.offsetX
-          const y = e.offsetY
-          this.mousemovex = x
-          this.mousemovey = y
-          console.log('mousemovex', this.mousemovex)
-          console.log('mousemovey', this.mousemovey)
-          this.xx = parseInt(this.mousemovex - this.cx)
-          this.yy = parseInt(this.mousemovey - this.cy)
-          // console.log('xx', this.xx)
-          // console.log('yy', this.yy)
-          // const radians = Math.atan2(e.pageY - window.innerHeight * 0.5, e.pageX - window.innerWidth * 0.5)
-          // const deg = radians * (180 / Math.PI)
-          // console.log('deg', deg)
-          // this.rotate = deg
-        } else if (type === 'up') {
-          this.active = false
-        }
-      }
-    },
-    getMousePos1 (event) {
-      this.getMousePos2()
-    },
-    getMousePos2 (event) {
-      const e = event || window.event
-      const scrollX = document.documentElement.scrollLeft || document.body.scrollLeft
-      const scrollY = document.documentElement.scrollTop || document.body.scrollTop
-      const x = e.pageX || e.clientX + scrollX
-      const y = e.pageY || e.clientY + scrollY
-      console.log(x)
-      console.log(y)
-      // this.cx = x
-      // this.cy = y
+    handlePreview (file) {
+      console.log(file);
     }
   }
 }
 </script>
-
-<style scoped >
-.kkk {
-  position: absolute;
-  top: 10%;
-  left: 25%;
-  width: 50%;
-  height: 80%;
-  background: orange;
-}
-.content {
-  position: relative;
-
-  background: orange;
-}
-.box {
-  width: 500px;
-  height: 50px;
-  background: blue;
-}
-</style>
