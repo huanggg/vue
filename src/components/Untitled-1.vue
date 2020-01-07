4096f8a5487730b7619bda1d600f9116916b4c33
qiyue <292019984@qq.com>
2020-01-07 21:20:31 +0800

    555555

---
+++ src/components/Untitled-1.vue
@@ -0,0 +1,127 @@
+option = {
+    title: {
+        text: '堆叠区域图'
+    },
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            type: 'cross',
+            label: {
+                backgroundColor: '#6a7985'
+            }
+        }
+    },
+    legend: {
+        data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+    },
+    toolbox: {
+        feature: {
+            saveAsImage: {}
+        }
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    xAxis: [
+        {
+            type: 'category',
+            boundaryGap: false,
+            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+        }
+    ],
+     yAxis: [ //两个y轴  左边y轴
+                    {
+                        type: 'value',
+                        name: "标准保费",
+
+                        axisLabel: {
+                            show: true,
+                            interval: 'auto',
+                            formatter: '{value}% '
+                        },
+                        splitNumber: 10,
+                        splitLine: {
+                            show: true,
+                            lineStyle: {
+                                type: 'dashed'
+                            }
+                        },
+                        splitArea: {
+                            show: false
+                        },
+                        // max: 100,
+                        // interval: 10,
+                    },
+                   
+                    //右边y轴
+                    {
+                        type: 'value',
+                        name: "增长",
+                        axisLabel: {
+                            show: true,
+                            interval: 'auto',
+                            formatter: '{value}'
+                        },
+                        splitNumber: 10,
+                        splitLine: {
+                            show: true,
+                            lineStyle: {
+                                type: 'dashed'
+                            }
+                        },
+                        splitArea: {
+                            show: false
+                        },
+                        // max: 100,
+                        // interval: 10,
+                    }
+                ],
+    series: [
+        {
+            name: '邮件营销',
+            type: 'line',
+            stack: '总量',
+         
+               yAxisIndex: '1',  
+            data: [120, 132, 101, 134, 90, 230, 210]
+        },
+         
+        {
+            name: '联盟广告',
+            type: 'line',
+            stack: '总量',
+          
+            data: [220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+            name: '视频广告',
+            type: 'line',
+            stack: '总量',
+           
+            data: [150, 232, 201, 154, 190, 330, 410]
+        },
+        {
+            name: '直接访问',
+            type: 'line',
+            stack: '总量',
+           
+            data: [320, 332, 301, 334, 390, 330, 320]
+        },
+        {
+            name: '搜索引擎',
+            type: 'line',
+            stack: '总量',
+            label: {
+                normal: {
+                    show: true,
+                    position: 'top'
+                }
+            },
+           
+            data: [820, 932, 901, 934, 1290, 1330, 1320]
+        }
+    ]
+};
