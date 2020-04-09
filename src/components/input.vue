522fc1141f9809eb3262d38f15fa7b1f4c9c1ac7
huanggg <292019984@qq.com>
2020-04-09 13:55:31 +0800

    Create input.vue

---
+++ src/components/input.vue
@@ -0,0 +1,10 @@
+  keyup(e) {
+            var that = this
+            if (e.keyCode != 38 || e.keyCode != 40 || e.keyCode != 13) {
+                clearTimeout(window.settime)
+                window.settime = setTimeout(function () {
+                    that.Obtain()
+                }, 2000)
+
+            }
+        },
