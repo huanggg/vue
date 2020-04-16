f622ec0229866adf20e1fd31b388f13f0fd6e1b7
huanggg <292019984@qq.com>
2020-04-16 17:10:40 +0800

    Create NavMenu.vue

---
+++ NavMenu.vue
@@ -0,0 +1,110 @@
+<template>
+    <div class="navMenu">
+        <template v-for="(navMenu) in navMenus">
+            <!-- 最后一级菜单 -->
+            <el-menu-item
+                v-if="!navMenu.childs"
+                :key="navMenu.id"
+                :index="navMenu.id"
+                :title="navMenu.menuName"
+                @click="changeTab(navMenu)"
+            >
+                <!-- <i :class="navMenu.entity.icon"></i> // 图标  -->
+                <span slot="title">{{navMenu.menuName}}</span>
+            </el-menu-item>
+            <!-- 此菜单下还有子菜单 -->
+            <el-submenu
+                v-if="navMenu.childs"
+                :key="navMenu.id"
+                :index="navMenu.id"
+                :title="navMenu.menuName"
+                @click="changeTab(navMenu)"
+            >
+                <template slot="title">
+                    <!-- <i :class="navMenu.entity.icon"></i> -->
+                    <span slot="title">{{navMenu.menuName}}</span>
+                </template>
+                <!-- 递归 -->
+                <NavMenu :navMenus="navMenu.childs"></NavMenu>
+            </el-submenu>
+        </template>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'NavMenu',
+    props: ['navMenus'],
+    data() {
+        return {}
+    },
+    mounted() {
+
+
+    },
+    computed: {
+
+    },
+    methods: {
+        changeTab(item) {
+
+        },
+
+    }
+}
+</script>
+
+<style scoped>
+.el-menu-item.is-active {
+  background: #2772b2 !important;
+}
+/* 水平样式 */
+.el-menu--horizontal > div > .el-submenu {
+  float: left;
+}
+/* 一级菜单的样式 */
+.el-menu--horizontal > div > .el-menu-item {
+  float: left;
+  height: 60px;
+  line-height: 60px;
+  margin: 0;
+  border-bottom: 2px solid transparent;
+  color: #909399;
+}
+/* 解决下图1 下拉三角图标 */
+.el-menu--horizontal > div > .el-submenu .el-submenu__icon-arrow {
+  position: static;
+  vertical-align: middle;
+  margin-left: 8px;
+  margin-top: -3px;
+}
+/* 解决下图2 无下拉菜单时 不对齐问题 */
+.el-menu--horizontal > div > .el-submenu .el-submenu__title {
+  height: 60px !important;
+  line-height: 60px !important;
+  border-bottom: 2px solid transparent;
+  color: #909399;
+}
+</style>
+<style >
+.el-menu--horizontal .el-submenu .el-submenu__title {
+  height: 45px !important;
+  line-height: 45px !important;
+  border-bottom: 2px solid transparent;
+  color: #909399;
+}
+.el-menu--horizontal > .el-submenu.is-active .el-submenu__title {
+  border-bottom: 0px !important;
+  color: #2196f3 !important;
+}
+
+.el-submenu__title {
+  padding: 0 22px !important;
+}
+.el-submenu__title:hover {
+  background: white !important;
+  color: #1976d3 !important;
+}
+</style>
+
